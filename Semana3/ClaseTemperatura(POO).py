264befb162a09d62f921fb9f079d3e89cadfe473
uea-jose <jv.avilar@uea.edu.ec>
2024-06-11 20:28:16 -0500

    v15

--- Semana3/ClaseTemperatura(POO).py
+++ Semana3/ClaseTemperatura(POO).py
@@ -3,26 +3,30 @@ class ClimaDiario:
     def __init__(self,dias): #constructor con parametro de entra dias
         self.dias = dias
         #ingresar datos por consola y limitar a nimero dias
-        self.temperatura =[0 for i in range(dias)]
-        self.dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+#2. ENCAPSULAMIENTO
+        self.__temperatura =[0 for i in range(dias)] #PRIVADO
+        self.__dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]#PRIVADO
     #defino metodo para el ingreso de datos
     def ingresarTemperatura(self):
         for i in range(self.dias):
-            dia = self.dias_semana[i]
-            self.temperatura[i] = float(input(f"Ingrese en °C la temperatura del {dia}: "))
+            dia = self.__dias_semana[i]
+            self.__temperatura[i] = float(input(f"Ingrese en °C la temperatura del {dia}: "))
 
+    #Defino  metodos para acceder a los datos  privados encapsualdos
 
+#1. HERENCIA
 #defino clase calular temperatuira promedio
 # Defino la clase ClimaTemperatura que hereda de ClimaDiario
-class ClimaTemperatura(ClimaDiario):
-    # Limitar el ingreso de valores a 7 días
-    def __init__(self):
-        ClimaDiario.__init__(self, 7)
+
+    class ClimaTemperatura(ClimaDiario):
+        # Limitar el ingreso de valores a 7 días
+        def __init__(self):
+            super().__init__(7)  # Usando super() para llamar al constructor de la clase base
 
     # Método para calcular el promedio de temperaturas
     def calcularPromedio(self):
         # Calcular el promedio de las temperaturas
-        promedio = sum(self.temperatura) / self.dias
+        promedio = sum(self.__temperatura) / self.dias
         return promedio
 
 # Instancio objeto
